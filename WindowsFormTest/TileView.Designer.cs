683c843b0c7fc8b8858b7fb0e31e3cbdc77c50a8
Evans, Jeff L <Jeff.L.Evans@conocophillips.com>
2021-08-14 18:46:42 -0500

    update view

--- WindowsFormTest/TileView.Designer.cs
+++ WindowsFormTest/TileView.Designer.cs
@@ -34,7 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 761);
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1265, 939);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "TileView";
             this.Text = "TileView";
--- WindowsFormTest/TileView.cs
+++ WindowsFormTest/TileView.cs
@@ -8,28 +8,54 @@ namespace WindowsFormTest
     public partial class TileView : Form
     {
         public string Selection { get; set; }
-        private List<Frame> frameList;
+        private EPFReader epf;
+        private List<Frame> frameList;        
 
         public TileView()
         {
             InitializeComponent();
         }
+
         private void TileView_Load(object sender, EventArgs e)
         {
-            EPFReader epf = new EPFReader();
-            frameList = epf.ReadEPF(Selection);            
+            epf = new EPFReader();
+            frameList = epf.ReadEPF(Selection);
         }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            for(int i = 1; i < frameList.Count; i++)
+            var maxSize = GetMaxFrameSize();
+            for (int i = 0; i < frameList.Count; i++)
             {
-                int column = (int)Math.Floor(i / 16f);
-                int row = i % 16;
+                int row = (int)Math.Floor(i / 16f);
+                int column = i % 16;
                 Bitmap bitmap = EPFRenderer.RenderEPF(frameList[i]);
-                Rectangle r = new Rectangle(row * bitmap.Width, column * bitmap.Height, bitmap.Width, bitmap.Height);
+                Rectangle r = new Rectangle(column * maxSize, row * maxSize, bitmap.Width, bitmap.Height);
                 e.Graphics.DrawImage(bitmap, r);
             }
+        }
+
+        private int GetMaxFrameSize()
+        {
+            var size = 0;
+
+            for (var i = 0; i < frameList.Count; i++)
+            {
+                Bitmap bitmap = EPFRenderer.RenderEPF(frameList[i]);
+
+                if (bitmap.Width > size)
+                {
+                    size = bitmap.Width;
+                }
+
+                if (bitmap.Height > size)
+                {
+                    size = bitmap.Height;
+                }
+            }
+
+            return size;
         }
     }
 }
